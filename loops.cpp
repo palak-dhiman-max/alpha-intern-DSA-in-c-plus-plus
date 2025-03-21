078b7a688e0df8b0963ca040a60b3d036a63859b
palak-dhiman-max <uk1211417@gmail.com>
2025-03-21 19:30:46 +0530

    loops

---
+++ loops.cpp
@@ -0,0 +1,82 @@
+# include <iostream>
+using namespace std;
+int main() {
+  // q-1  print odd number between 1. and 100
+ /* for(int i= 1; i <=100; i++){
+      if(i%2!=0){
+          cout<<i<<endl;
+      }
+  
+  }*/
+  //  q-2  print  number b/w 1/100 which is divisible by 3
+ /* for(int i= 1; i <=100; i++){
+      if(i%3==0){
+          cout<<i<<endl;
+      }
+      
+  }*/
+  // q-3   display ap 4,7,10,13....
+  /*int n;
+  cin>>n;
+  int a= 4;
+  for(int i=1; i<=n;i++){
+      cout<<a<<endl;
+      a=a+3;
+  }*/
+  
+  // q-4   display gp 3,12,48
+  /*int n; 
+  cin>>n;
+  int a=3;
+  for(int i=1; i<=n ;i++){
+    cout<<a<<endl;
+    a=a*4;
+  }
+  */
+  
+  //q-5  wap to print all ascii values  and corresponding character using while loop
+  
+ /* char ch;
+  cin>>ch;
+  int ch1 = ch;
+  int n = 1;
+  cout<<"enter capital  alphabet a: ";
+  while (ch1 >=65 &&ch1 <=90){
+      cout<<ch1<<"--"<<ch<<endl;
+      ch1++;
+      ch++;
+  }*/
+  /*
+      char ch;
+  cin>>ch;
+  int ch1 = ch;
+  int n = 1;
+  cout<<"enter small alphabet a: ";
+  while (ch1 >=97 &&ch1 <=122){
+      cout<<ch1<<"--"<<ch<<endl;
+      ch1++;
+      ch++;
+  }*/
+  
+  
+  // q-6 print number n to 1
+ /* int n ;
+  cin>>n;
+  for(int i=n ;i >=0; i--){
+      cout<<i<< endl;
+  }*/
+  
+  //q-7  wap to print sum of n natural number
+  /*int n;
+  cin>>n;
+  int sum = 0;
+  for(int i= 1; i <=n ; i ++){
+      sum = sum + i ;
+  }cout<<sum;*/
+  
+  
+  
+   
+
+    
+}
