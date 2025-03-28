e0bf8241f2214ef0ddfdade6fab8da13fa774b37
palak-dhiman-max <uk1211417@gmail.com>
2025-03-28 20:49:33 +0530

    new pattern

--- advancepattren.cpp
+++ advancepattren.cpp
@@ -32,23 +32,33 @@ cout<<" ";
      }
 */
      // 
-  
-     cout<<"enter n: ";
-     int n;
-    
-      cin>>n;
-      for(int i=0; i<n ; i++){
-         for(int j=1; j<=n-i; j++){
- cout<<j;
-         }
-         for(int j=1; j<=2*i-1; j++){
-             cout<<" ";
-                     }
+ 
 
-        for(int p=1; p<=n-i; p++){
-            cout<<p;
-            
-        }
-                     cout<<endl;
-      }
+     // palindrome pyramid pattern 
+    /* cout<<"enter n: ";
+      int n;
+     
+       cin>>n;
+       for(int i=1; i<=n ; i++){
+
+          for(int j=1; j<=n-i; j++){
+              cout<<" ";
+                      }
+              
+ 
+         for(int p=1; p<=i; p++){
+             cout<<p;
+             
+         }
+
+         for(int k=i-1; k>=1; k--){
+                cout<<k;
+                        }
+                      cout<<endl;
+       }*/
+
+
+
+
+
  }
