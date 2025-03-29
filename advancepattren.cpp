29ca989da3612897efbb2442a6c8a444a23b4a3e
palak-dhiman-max <uk1211417@gmail.com>
2025-03-29 17:29:04 +0530

    new code

--- advancepattren.cpp
+++ advancepattren.cpp
@@ -58,7 +58,61 @@ cout<<" ";
        }*/
 
 
+/*
+    1234321
+    123 321
+    12   21
+    1     1
+                  */
+   /* int n;
+    cin>>n;
+    for(int i=0; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<<j;
+        }
+        
+    
+    
+    for(int k=1; k<=2*i-1;k++){
+        cout<<" ";
+    }
+    
+        for(int p=n-i;p>=1; p--){
+          if(p==n){continue;}
+            cout<<p;
+        }
+       cout<<endl; 
+    }*/
 
 
+/*
+ABCDEFG
+ABC EFG 
+AB   FG 
+A     G
 
- }
+*/
+    
+   /* int n;
+    cin>>n;
+    for(int i=0; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<<char(j+64);
+        }
+        
+    
+    
+    for(int k=1; k<=2*i-1;k++){
+        cout<<" ";
+    }
+    
+        for(int p=1;p<=n-i; p++){
+          if(p==n){continue;}
+            cout<<char(p+68);
+        }
+       cout<<endl; 
+    }*/
+}
+
+
+ 
