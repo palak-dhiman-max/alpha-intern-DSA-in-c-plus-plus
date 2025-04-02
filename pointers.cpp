54d1ebcd693c5e5711fef18b91258a96d40873f2
palak-dhiman-max <uk1211417@gmail.com>
2025-04-02 20:16:55 +0530

    pointer

---
+++ pointers.cpp
@@ -0,0 +1,48 @@
+# include <iostream>
+using namespace std;
+int main (){
+    // q-1 find product of tow nu,ber using pointer
+
+   /* int a,b;
+   cin>>a>>b;
+    int *ptr1= &a;
+    int *ptr2= &b;
+    int mul;
+    mul = (*ptr1) * ( *ptr2);
+    cout<<mul;
+*/
+
+     // q-2 int *p,q;
+    // p is a pointer and q is an integer
+
+
+    //q-3  find output of code snippet
+   /* int a=10 ;
+    int b=20;
+    int *ptr = &a;
+    b=*ptr+1;
+    ptr= &b;
+    cout<<*ptr<<" "<<a<<" "<<b;*/
+
+
+   // output->  11 10 11
+
+
+   // q-4 
+   /*int a=15;
+   int b=20;
+   int *ptr= &a;
+   int *ptr2 = &b;
+   *ptr= *ptr2;
+   cout<<*ptr;
+*/
+   // a get value of b
+
+   //q-5  
+  /* int a=10;
+   int b=20;
+   int *ptr;
+   *ptr=5;*/
+    // it is not correct
+
+}
